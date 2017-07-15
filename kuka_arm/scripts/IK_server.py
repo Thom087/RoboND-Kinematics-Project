b10debcd042518b306fa251fe716cb965a18fbe6
Thomas Schatt <thomas.schatt@bluewin.ch>
2017-07-15 16:25:17 +0200

    check coordinate system, correction matrix

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -109,11 +109,11 @@ def handle_calculate_IK(req):
                            [sin(q7)*sin(alpha6),cos(q7)*sin(alpha6), cos(alpha6), cos(alpha6)*d7],
                            [		      0,		  0,	       0,	       1]])
 
-            T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
+            #T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
 
-            T0_3 = T0_1 * T1_2 * T2_3
+	    T0_3 = T0_1 * T1_2 * T2_3
 
-            # Get rotation matrix R0_3
+	    # Get rotation matrix R0_3
             R0_3 = T0_3[0:3, 0:3]
 
             #correction matrix
@@ -127,12 +127,12 @@ def handle_calculate_IK(req):
                           [       0, 	       0, 	   1]])
             R_corr = R_z*R_y
 
-	    ta = Matrix([[0], [0], [0]])
-	    th = Matrix([[0, 0, 0, 1]])
+	    #ta = Matrix([[0], [0], [0]])
+	    #th = Matrix([[0, 0, 0, 1]])
 
-	    T_corr = (R_corr.row_join(ta)).col_join(th)
+	    #T_corr = (R_corr.row_join(ta)).col_join(th)
 
-            T_total = T0_G * T_corr
+            #T_total = T0_G * T_corr
 
 
             # Extract end-effector position and orientation from request
@@ -146,8 +146,9 @@ def handle_calculate_IK(req):
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                  req.poses[x].orientation.z, req.poses[x].orientation.w])
 
-            print("px: ", px, ", py: ", py, ", pz: ", pz)
-            print("roll: ", roll, ", pitch: ", pitch, ", yaw: ", yaw)
+            print("Requested poses")
+            print("x", px, "y", py, "z", pz)
+            print("roll", roll, "pitch", pitch, "yaw", yaw)
 
             # Calculate joint angles using Geometric IK method
 
@@ -171,45 +172,42 @@ def handle_calculate_IK(req):
             py_wc = py - d67*Rrpy[1,0]
             pz_wc = pz - d67*Rrpy[2,0]
             p_wc  = Matrix([[px_wc], [py_wc], [pz_wc]])
-	       
+	    print("p_wc: ", p_wc)   
 
             # Calculate joint theta 1
             theta1 = mpmath.atan2(py_wc,px_wc)
-	    theta1_big   = 2*mpmath.pi-theta1
-	    print("theta1: ", theta1, ", theta1_deg: ", theta1*rtd )
-	    print("theta1_big: ", theta1_big, ", theta1_big_deg: ", theta1_big*rtd)
+	    theta1_big   = 2*pi-theta1
+	    #print("theta1_big: ", theta1_big, ", theta1_big_deg: ", theta1_big*rtd)
 
             # Calculate joint theta 3
             p_02 = Matrix([[a12*cos(theta1)], [a12*sin(theta1)], [d01]])
             p_25 = p_wc - p_02
             #								1.25     -0.054   1.5
             D = (a23**2 + a34**2 + d34**2 -p_25[0]**2 - p_25[2]**2)/(2*a23*sqrt(a34**2 + d34**2))
-	    print("D: ", D)
 	    #atan2(GK->y, AK->x)
 	    gamma = atan2(sqrt(1-D**2),D)
 	    delta = atan2(0.054, 0.96)-atan2(0.054, 1.5)
 	    theta3 = -np.pi/2 + gamma - delta
 	    theta3_big = np.pi - theta3 	#check which one is wright
-	    print("theta3: ", theta3, ", theta3_deg: ", theta3*rtd )
-	    print("theta3_big: ", theta3_big, ", theta3_big_deg: ", theta3_big*rtd)
+	    #print("theta3_big: ", theta3_big, ", theta3_big_deg: ", theta3_big*rtd)
 
             # Calculate joint theta 2
 	    delta2=atan2(p_25[2],p_25[0])
 	    E = (a23**2 +p_25[0]**2 + p_25[2]**2 - a34**2 - d34**2)/(2*a23*sqrt(p_25[0]**2 + p_25[2]**2))  
 	    delta3 = atan2(sqrt(1-E**2),E)
-	    theta2 = delta2 + delta3
-	    print("theta2: ", theta2*rtd)
+	    theta2 = delta2 + delta3 - pi/2
+	    #print("theta2: ", theta2*rtd)
 
             # Calculate numerical R0_3_num
             R0_3_num = R0_3.evalf(subs={q1:theta1, q2:theta2, q3:theta3})
 
             # Calculate R_3_6
-            # R3_6 = R0_3_num.transpose() * Rrpy 
-            R3_6 = (R0_3_num**(-1)) * Rrpy
+            # R3_6 = (R0_3_num**(-1)) * Rrpy 
+            R3_6 = R0_3_num.transpose() * Rrpy * R_corr
 
             # Get Euler angles theta 4-6
             # Rotation about x -> theta 4
-            theta4 = atan2(R3_6[2,1],R3_6[2,2])
+            theta4 = atan2(R3_6[2,1],R3_6[2,2])+pi/2
 
             # Rotation about z -> theta 5
             theta5 = atan2(R3_6[1,0],R3_6[0,0])
@@ -217,22 +215,19 @@ def handle_calculate_IK(req):
             # Rotation about y -> theta 6
             theta6 = atan2(-R3_6[2,0], sqrt(R3_6[0,0]**2+R3_6[1,0]**2))
 
-
-            #theta6 = atan2(R3_6[1,0],R3_6[0,0]) # rotation about Z-axis
-            #theta5 = atan2(-R3_6[2,0], sqrt(R3_6[0,0]*R3_6[0,0]+R3_6[1,0]*R3_6[1,0])) # rotation about Y-axis
-            #theta4 = atan2(R3_6[2,1],R3_6[2,2]) # rotation about X-axis
-
+	    print("theta1: ", theta1*rtd)
+	    print("theta2: ", theta2*rtd)
+	    print("theta3: ", theta3*rtd)
+	    print("theta4: ", theta4*rtd)
+	    print("theta5: ", theta5*rtd)
+	    print("theta6: ", theta6*rtd)
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
             joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
             joint_trajectory_list.append(joint_trajectory_point)
 
-            print("Requested poses")
-            print("x", px, "y", py, "z", pz)
-            print("roll", roll, "pitch", pitch, "yaw", yaw)
-
-            T0_G_num = T0_G.evalf(subs={q1:theta1, q2:theta2, q3:theta3, q4:theta4, q5:theta5, q6:theta6})
+            #T0_G_num = T0_G.evalf(subs={q1:theta1, q2:theta2, q3:theta3, q4:theta4, q5:theta5, q6:theta6})
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
